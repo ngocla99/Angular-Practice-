843f00617824f04de381ebdb7a9fb8852c1c7674
Anh-Ngoc Le <87078780+ngocla99@users.noreply.github.com>
2021-11-12 20:48:01 +0700

    ⚡ 2nd commit

--- email-app - v1/src/app/email/email-preview/email-preview.component.ts
+++ email-app - v1/src/app/email/email-preview/email-preview.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Message } from '../email.model';
 import { EmailService } from '../email.service';
 
@@ -8,21 +9,42 @@ import { EmailService } from '../email.service';
   templateUrl: './email-preview.component.html',
   styleUrls: ['./email-preview.component.css'],
 })
-export class EmailPreviewComponent implements OnInit {
+export class EmailPreviewComponent implements OnInit, OnDestroy {
   messagesByUser!: Message[];
   messagesByFolder!: Message[];
   users!: string[];
   userId!: number;
+  userChanged: Subscription;
   folder!: string;
+  activeMessage!: string;
+
   constructor(
     private emailService: EmailService,
+    private router: Router,
     private route: ActivatedRoute
-  ) {}
+  ) {
+    this.userChanged = this.emailService.updateUser.subscribe((id: number) => {
+      this.userId = id;
+    });
+  }
 
   ngOnInit(): void {
+    this.emailService.updateUser.subscribe((id: number) => {
+      this.userId = id;
+      this.route.params.subscribe((params: Params) => {
+        this.folder = params['subject'];
+        this.emailService.updateActiveFolder.next(this.folder);
+        this.messagesByUser = this.emailService.getMessagesByUser(this.userId);
+        this.messagesByFolder = this.emailService.getMessagesByFolder(
+          this.folder,
+          this.messagesByUser
+        );
+      });
+    });
+
     this.route.params.subscribe((params: Params) => {
-      this.userId = +params['userId']; // NOT work
       this.folder = params['subject'];
+      this.emailService.updateActiveFolder.next(this.folder);
       this.messagesByUser = this.emailService.getMessagesByUser(this.userId);
       this.messagesByFolder = this.emailService.getMessagesByFolder(
         this.folder,
@@ -31,5 +53,16 @@ export class EmailPreviewComponent implements OnInit {
     });
 
     this.users = this.emailService.getUsers();
+  }
+
+  onShowMessage(id: string) {
+    this.activeMessage = id;
+    this.router.navigate([{ outlets: { detail: id } }], {
+      relativeTo: this.route.parent,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userChanged.unsubscribe();
   }
 }
