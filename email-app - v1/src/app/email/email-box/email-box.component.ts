17b309a6af33609dbdfa58ee72b54ff1f3a11cfc
Anh-Ngoc Le <87078780+ngocla99@users.noreply.github.com>
2021-11-12 20:49:15 +0700

    ⚡ 2nd commit

--- email-app - v1/src/app/email/email-box/email-box.component.ts
+++ email-app - v1/src/app/email/email-box/email-box.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-box',
   templateUrl: './email-box.component.html',
-  styleUrls: ['./email-box.component.css']
+  styleUrls: ['./email-box.component.css'],
 })
-export class EmailBoxComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class EmailBoxComponent implements OnInit, OnDestroy {
+  activeFolder!: string;
+  folders!: string[];
+  folderChanged!: Subscription;
+  userChanged!: Subscription;
+  userId!: number;
+  constructor(private router: Router, private emailService: EmailService) {
+    this.userChanged = this.emailService.updateUser.subscribe(
+      (userId: number) => {
+        this.userId = userId;
+      }
+    );
+    this.folderChanged = this.emailService.updateActiveFolder.subscribe(
+      (folder: string) => {
+        this.activeFolder = folder;
+      }
+    );
   }
 
+  ngOnInit(): void {
+    this.folders = this.emailService.getFolders();
+  }
+
+  onClick(folder: string) {
+    this.router.navigateByUrl('/messages/' + this.userId + '/' + folder);
+  }
+
+  ngOnDestroy(): void {
+    this.folderChanged.unsubscribe();
+    this.userChanged.unsubscribe();
+  }
 }
