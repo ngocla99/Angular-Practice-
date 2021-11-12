5b95a7cc10a2d20373acc573ba1e4862f5b056cc
Anh-Ngoc Le <87078780+ngocla99@users.noreply.github.com>
2021-11-12 20:48:48 +0700

    ⚡ 2nd commit

--- email-app - v1/src/app/email/email-detail/email-detail.component.ts
+++ email-app - v1/src/app/email/email-detail/email-detail.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Message } from '../email.model';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-detail',
   templateUrl: './email-detail.component.html',
-  styleUrls: ['./email-detail.component.css']
+  styleUrls: ['./email-detail.component.css'],
 })
 export class EmailDetailComponent implements OnInit {
-
-  constructor() { }
+  message!: Message;
+  id!: string;
+  constructor(
+    private route: ActivatedRoute,
+    private emailService: EmailService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id'];
+      this.message = this.emailService.getMessageById(this.id);
+    });
   }
-
 }
