8a4116e9d973f3a5756e08a244e3937fbf0050b2
Anh-Ngoc Le <87078780+ngocla99@users.noreply.github.com>
2021-11-13 19:39:39 +0700

    ⚡ 2nd commit

--- email-app - v1/src/app/email/email.service.ts
+++ email-app - v1/src/app/email/email.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import messagesList from '../messages.json';
 import { Message } from './email.model';
 
@@ -6,20 +7,45 @@ import { Message } from './email.model';
   providedIn: 'root',
 })
 export class EmailService {
+  private folders: string[] = [
+    'inbox',
+    'finance',
+    'travel',
+    'personal',
+    'spam',
+    'drafts',
+    'sent',
+  ];
+
   private messages: Message[] = messagesList;
+
+  updateUser = new Subject<number>();
+  updateActiveFolder = new Subject<string>();
   construction() {}
+
+  getFolders() {
+    return [...this.folders];
+  }
 
   getUsers() {
     return [...new Set(this.messages.map((message) => message.to))];
   }
 
+  getUser(userId: number) {
+    return this.getUsers()[userId];
+  }
+
   getMessagesByUser(userId: number) {
     return this.messages.filter(
-      (message) => message.to === 'myself@angular.dev'
+      (message) => message.to === this.getUser(userId)
     );
   }
 
   getMessagesByFolder(folderName: string, messages: Message[]) {
     return messages.filter((message) => message.folder === folderName);
+  }
+
+  getMessageById(id: string) {
+    return this.messages.filter((message) => message._id === id)[0];
   }
 }
